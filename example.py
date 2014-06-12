8de2d0aaa210e66288a183373667a48c4109ac41
SamuraiT <t.yasukawa01@gmail.com>
2014-06-12 21:48:05 +0900

    add example file

---
+++ example.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# coding: utf-8
+"""
+Copyright 2014 Tatsuro Yasukawa.
+
+Distributed under the GNU General Public License at
+gnu.org/licenses/gpl.html.
+"""
+
+
+def main():
+    print('Enjoy pprint2!')
+
+if __name__ == '__main__':
+    main()
+
